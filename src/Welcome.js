5ffa0770cbc47d11e94e651b90629fe6fb64b76c
Aashish Panthi <aashishpanthi11@gmail.com>
2022-04-20 12:38:45 +0545

    login as page style changed

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,88 +1,127 @@
-import React from 'react';
+import React from "react";
+import {
+  Button,
+  View,
+  StyleSheet,
+  Text,
+  Platform,
+  StatusBar,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+} from "react-native";
 
-import {Button, View,StyleSheet,Text,Platform,StatusBar,SafeAreaView,Image,TouchableOpacity} from 'react-native';
-import { useFonts,   Poppins_800ExtraBold,
-    Poppins_100Thin,
+import {
+  useFonts,
+  Poppins_800ExtraBold,
+  Poppins_100Thin,
+  Poppins_500Medium,
+} from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
+
+function Welcome({ navigation }) {
+  let [fontsLoaded] = useFonts({
+    Poppins_800ExtraBold,
     Poppins_500Medium,
-    } from  '@expo-google-fonts/poppins';
-import AppLoading from 'expo-app-loading';
-function Welcome({navigation}) {
-    let [fontsLoaded] = useFonts({
-        Poppins_800ExtraBold,
-        Poppins_500Medium,
-        Poppins_100Thin,
+    Poppins_100Thin,
+  });
 
-     });
-   
-     if (!fontsLoaded) {
-       return <AppLoading />;
-     }
-    return (
-       <SafeAreaView style={styles.maincontainer} > 
-       
-       <Text style={styles.head}>Login as</Text>
-           <TouchableOpacity style={styles.container} onPress={()=>navigation.navigate('Loginp')}>
-              <Image style={styles.img} resizeMode='center' source={require('../assets/parent1.png')}/>
-               <Text style={styles.txt}>A parent</Text>
-               </TouchableOpacity>
-            
-           <TouchableOpacity style={styles.containerr} onPress={()=>navigation.navigate('Logint')}>
-           <Image style={styles.img} resizeMode='center' source={require('../assets/teacher.png')}/>
-               <Text style={styles.txt}>A Teacher</Text>
-           </TouchableOpacity>
-       </SafeAreaView> 
-    );
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <SafeAreaView style={styles.maincontainer}>
+      <Text style={styles.head}>Login as</Text>
+
+      {Platform.OS === "android" ? (
+        <>
+          <TouchableNativeFeedback
+            onPress={() => navigation.navigate("Loginp")}
+          >
+            <View style={styles.container}>
+              <Image
+                style={styles.img}
+                resizeMode="contain"
+                source={require("../assets/parent1.png")}
+              />
+              <Text style={styles.txt}> A parent</Text>
+            </View>
+          </TouchableNativeFeedback>
+          <TouchableNativeFeedback
+            onPress={() => navigation.navigate("Logint")}
+          >
+            <View style={styles.container}>
+              <Image
+                style={styles.img}
+                resizeMode="contain"
+                source={require("../assets/teacher.png")}
+              />
+              <Text style={styles.txt}>A teacher</Text>
+            </View>
+          </TouchableNativeFeedback>
+        </>
+      ) : (
+        <>
+          <TouchableOpacity onPress={() => navigation.navigate("Loginp")}>
+            <View style={styles.container}>
+              <Image
+                style={styles.img}
+                resizeMode="center"
+                source={require("../assets/parent.png")}
+              />
+              <Text style={styles.txt}>A parent</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => navigation.navigate("Logint")}>
+            <View style={styles.container}>
+              <Image
+                style={styles.img}
+                resizeMode="contain"
+                source={require("../assets/teacher.png")}
+              />
+              <Text style={styles.txt}>A Teacher</Text>
+            </View>
+          </TouchableOpacity>
+        </>
+      )}
+    </SafeAreaView>
+  );
 }
-const styles=StyleSheet.create({
-    head:{
-        fontSize:50,
-        color:'#222F5B',
-        fontFamily: 'Poppins_800ExtraBold',
-        
-    },
-    maincontainer:{
-        height:'100%',
-        width: '100%',
-        paddingTop: Platform.OS==='android' ? StatusBar.currentHeight:0,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        flexDirection: 'column'
-        
-        
-    },
-    container:{
-        height: '30%',
-        width: '60%',
-        backgroundColor: '#fff',
-        elevation: 5,
-        shadowColor: '#000000',
-        borderRadius: 20,
-           
-        
-   
-    },
-    containerr:{
-        height: '30%',
-        width: '60%',
-        backgroundColor: '#fff',
-        shadowColor: '#000000',
-        borderRadius: 20,
-         elevation: 5,
-         
-    },
-    img:{
-        width:'100%',
-        height:'70%',
-        alignSelf:'center',
-    },
-    txt:{
-        textAlign:'center',
-        marginBottom:10,
-        fontFamily: 'Poppins_500Medium',
-        fontWeight:'900',
-        color:'#222F5B',
-        fontSize:20,
-    },
+const styles = StyleSheet.create({
+  container: {
+    height: 200,
+    width: 200,
+    borderRadius: 25,
+    backgroundColor: "#fff",
+    shadowColor: "#666",
+    elevation: 25,
+  },
+  head: {
+    fontSize: 48,
+    color: "#222F5B",
+    fontFamily: "Poppins_800ExtraBold",
+  },
+  img: {
+    marginTop: 25,
+    height: 125,
+    alignSelf: "center",
+  },
+  maincontainer: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    flexDirection: "column",
+  },
+  txt: {
+    textAlign: "center",
+    fontFamily: "Poppins_500Medium",
+    color: "#222F5B",
+    fontSize: 20,
+    marginTop: 8,
+  },
 });
 
 export default Welcome;
