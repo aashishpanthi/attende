e5d0ccc78ef91fd726ce962b31a9dd78058a5ae7
Aashish Panthi <aashishpanthi11@gmail.com>
2022-05-21 19:21:23 +0545

    dynamic attendance information

--- src/screens/Attendanceinfo.js
+++ src/screens/Attendanceinfo.js
@@ -1,12 +1,84 @@
-import { View, Text, Image, Pressable, Linking, Picker } from "react-native";
+import { View, Text, Image, Pressable, Linking } from "react-native";
 import styles from "../styles/student_info";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faPhone, faHome } from "@fortawesome/free-solid-svg-icons";
 import colors from "../../config/colors";
+import { useState, useEffect } from "react";
 
-const Attendanceinfo = ({ navigation, route }) => {
+import { db } from "../../config/firebase";
+import { getDocs, collection, query, where } from "firebase/firestore";
+
+const Attendanceinfo = ({ route }) => {
   const { student } = route.params;
-  const { name, image, section, class: grade, phone_no, address } = student;
+  const { id, name, image, section, class: grade, phone_no, address } = student;
+
+  const [presentDays, setPresentDays] = useState(0);
+  const [absentDays, setAbsentDays] = useState(0);
+  const [leaveDays, setLeaveDays] = useState(0);
+
+  const getAttendance = async () => {
+    try {
+      // query to fetch attendance data
+      const q = query(collection(db, "attendance"));
+
+      // fetching all the documents
+      const querySnapshot = await getDocs(q);
+
+      // extracting the data from the documents
+      const items = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+
+      let present = 0;
+      let absent = 0;
+
+      items.forEach((item) => {
+        if (item.presentStudents.includes(id)) {
+          // get present days
+          present++;
+        } else if (item.absentStudents.includes(id)) {
+          // get absent days
+          absent++;
+        }
+      });
+
+      setPresentDays(present); // set present days
+      setAbsentDays(absent); // set absent days
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const getLeaveDays = async () => {
+    try {
+      // query to fetch only student's leave request data
+      const q = query(
+        collection(db, "leave_appeals"),
+        where("student", "==", `${id}`),
+        where("status", "==", "approved")
+      );
+
+      // fetching all the documents
+      const querySnapshot = await getDocs(q);
+
+      // extracting the data from the documents
+      const items = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+
+      // setting the leave days
+      setLeaveDays(items.length);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    getAttendance();
+    getLeaveDays();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -52,15 +124,15 @@ const Attendanceinfo = ({ navigation, route }) => {
           <View style={styles.infobox}>
             <View style={styles.databox}>
               <Text style={styles.infotxt}>Present</Text>
-              <Text style={styles.datatxt}>7</Text>
-            </View>
-            <View style={styles.databox}>
-              <Text style={styles.infotxt}>Leave</Text>
-              <Text style={styles.datatxt}>1</Text>
+              <Text style={styles.datatxt}>{presentDays}</Text>
             </View>
             <View style={styles.databox}>
               <Text style={styles.infotxt}>Absent</Text>
-              <Text style={styles.datatxt}>0</Text>
+              <Text style={styles.datatxt}>{absentDays}</Text>
+            </View>
+            <View style={styles.databox}>
+              <Text style={styles.infotxt}>Leave</Text>
+              <Text style={styles.datatxt}>{leaveDays}</Text>
             </View>
           </View>
         </View>
